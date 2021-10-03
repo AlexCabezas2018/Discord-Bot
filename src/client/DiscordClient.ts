c1fad718ab2f9c58215fa089db0e915c1c660ec5
AlexCabezas2018 <alexcabezas0705@gmail.com>
2021-10-03 15:01:26 +0200

    More module changes

--- src/client/DiscordClient.ts
+++ src/client/DiscordClient.ts
@@ -1,8 +1,8 @@
 import {ApplicationClient} from "./ApplicationClient";
 import {Interaction} from "discord.js";
-import {InputMapper} from "./input/util/InputMapper";
-import {CommandController} from "./input/controller/CommandController";
-import {DiscordCommandOutputDisplay} from "./output/display/discord/DiscordCommandOutputDisplay";
+import {InputMapper} from "../service/io/input/util/InputMapper";
+import {CommandController} from "../service/io/input/controller/CommandController";
+import {DiscordCommandOutputDisplay} from "../service/io/output/display/discord/DiscordCommandOutputDisplay";
 const { Client, Intents } = require('discord.js');
 
 require("dotenv").config();
